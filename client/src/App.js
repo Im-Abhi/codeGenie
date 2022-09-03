917a719919099784793b5dc3592e789d49212f60
Im-Abhi <abhishekgupta942001@gmail.com>
2022-09-03 14:51:21 +0530

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import EditorPage from "./pages/EditorPage";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/editor/:roomId" element={<EditorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
