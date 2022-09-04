0f1938e547b60e65695a8f77705e56b7ea0a518b
hitesh <hitesh22rana@gmail.com>
2022-09-04 12:15:39 +0530

    backend changes

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -3,49 +3,49 @@ import styles from "./HeroSection.module.scss";
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
-  return (
-    <section>
-      <div className={styles.wrapper}>
-        <div>
-          <p>
-            Bring out the best in you & have safe and collaborative sessions
-            with
-            <span> Code Ginie</span>
-          </p>
-          <span>
-            Run your code with our fast paced IDE to get yourself rid of
-            nuisances of setting up your local development environment for every
-            single code you write. With our wide support of more than 3
-            languages Code Genie ensures you spend your time on coding rather
-            than on setting up development environment.
-          </span>
-          <Link to="/editor">
-            <button>Try IDE now</button>
-          </Link>
-        </div>
-        <div>
-          <img src="/hero.png" alt="hero" />
-        </div>
-      </div>
-      <div className={styles.glassMorphism}>
-        <h2>Our IDE provide integration for</h2>
-        <div className={styles.container}>
-          <div>
-            <img src="/c++.png" alt="cpp" />
-            <span>c++</span>
-          </div>
-          <div>
-            <img src="/python.png" alt="python" />
-            <span>python</span>
-          </div>
-          <div>
-            <img src="/js.png" alt="javascript" />
-            <span>javascript</span>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <div className={styles.wrapper}>
+                <div>
+                    <p>
+                        Bring out the best in you & have safe and collaborative
+                        sessions with
+                        <span> Code Ginie</span>
+                    </p>
+                    <p className={styles.subSection}>
+                        Run your code with our fast paced IDE to get yourself
+                        rid of nuisances of setting up your local development
+                        environment With our wide support of languages Code
+                        Genie ensures that you spend your time on coding rather
+                        than on setting up development environment.
+                    </p>
+                    <Link to="/editor">
+                        <button>Try our IDE now</button>
+                    </Link>
+                </div>
+                <div>
+                    <img src="/hero.png" alt="hero" />
+                </div>
+            </div>
+            <div className={styles.glassMorphism}>
+                <h2>Our IDE provide integration for</h2>
+                <div className={styles.container}>
+                    <div>
+                        <img src="/c++.png" alt="cpp" />
+                        <span>c++</span>
+                    </div>
+                    <div>
+                        <img src="/python.png" alt="python" />
+                        <span>python</span>
+                    </div>
+                    <div>
+                        <img src="/js.png" alt="javascript" />
+                        <span>javascript</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default HeroSection;
--- server/languages/cpp/main.cpp
+++ server/languages/cpp/main.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main(){
 freopen("input.txt", "r", stdin);
-    
-    cout << "Hello CPP!\n"; // prints Hello CPP!
     int n;
-    cin >> n;
-    cout << n;
+    cin>>n;
+    cout<<n*3<<endl;
+    cout << "Hello CPP!\n"; // prints Hello CPP!
     return 0;
 }
